b010801249929fb26eb34634a82310a19017a85c
Rohit <roohit1234@gmail.com>
2017-01-24 21:51:36 +0530

    navbar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   
 
   config.paperclip_defaults = {
-  storage: :s3,
+    storage: :s3,
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
